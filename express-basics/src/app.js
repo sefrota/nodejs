c0d4873663120dcd45390fdd916538d662659ae0
Sergio <sergio_letras@hotmail.com>
2015-12-05 17:09:43 -0500

    2ndcommit

---
+++ express-basics/src/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express = require('express'),
+	  posts = require('./mock/posts.json');
+
+var app = express();
+
+var postsLists = Object.keys(posts).map(function(value){
+	return posts[value];
+})
+
+app.use('/static',express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+	var path = req.path;
+	//res.locals.path = path
+	res.render('index' , {path: path});
+});
+
+app.get('/blog/:title?', function(req, res){ 
+	var title = req.params.title;
+	if (title === undefined) {
+		res.status(503);
+		res.render('blog', {posts: postsLists})
+	} else {
+		var post = posts[title] || {};
+		res.render('post', { post: post});
+	}
+});
+
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
+});
